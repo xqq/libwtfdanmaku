2e8c2c5703a5c6dceb8cc32aba428163b6b4071c
xqq <xqq@0ginr.com>
2015-08-21 18:50:30 +0800

    DanmakuFactory: Support multi-line danmaku by replace '/n'

--- src/DanmakuFactory.cpp
+++ src/DanmakuFactory.cpp
@@ -32,11 +32,23 @@ namespace WTFDanmaku {
         danmaku->mTimestamp = timestamp;
         danmaku->mDanmakuId = danmakuId;
         danmaku->mComment = comment;
+        ReplaceStringInplace(danmaku->mComment, L"/n", L"\r\n");
         danmaku->mTextSize = static_cast<float>(fontSize);
         danmaku->mTextColor = fontColor | 0xFF000000;
         danmaku->mTextShadowColor = (danmaku->mTextColor <= 0xFF000000) ? 0xFFFFFFFF : 0xFF000000;
 
         return danmaku;
+    }
+
+    void DanmakuFactory::ReplaceStringInplace(std::wstring& str, const wchar_t* search, const wchar_t* replace) {
+        size_t searchLength = wcslen(search);
+        size_t replaceLength = wcslen(replace);
+
+        size_t pos = 0;
+        while ((pos = str.find(search, pos)) != std::wstring::npos) {
+            str.replace(pos, searchLength, replace);
+            pos += replaceLength;
+        }
     }
 
 }
--- src/DanmakuFactory.hpp
+++ src/DanmakuFactory.hpp
@@ -14,6 +14,8 @@ namespace WTFDanmaku {
     class DanmakuFactory : public Noncopyable {
     public:
         static DanmakuRef CreateDanmaku(DanmakuType type, time_t time, std::wstring& comment, int fontSize, int fontColor, time_t timestamp = 0, int danmakuId = 0);
+    private:
+        static void ReplaceStringInplace(std::wstring& str, const wchar_t* search, const wchar_t* replace);
     };
 
 }
