8cc88d5265f1918abf1488f7a4c10fe84e16d35f
xqq <xqq@0ginr.com>
2015-08-22 20:47:24 +0800

    Renderable: Stupid text border style

--- src/Renderable.cpp
+++ src/Renderable.cpp
@@ -60,12 +60,16 @@ namespace WTFDanmaku {
         renderTarget->SetTextAntialiasMode(D2D1_TEXT_ANTIALIAS_MODE_GRAYSCALE);
 
         ComPtr<ID2D1SolidColorBrush> bottomBrush;
-        renderTarget->CreateSolidColorBrush(D2D1::ColorF(mDanmaku->mTextShadowColor, 0.9f), &bottomBrush);
-        renderTarget->DrawTextLayout(D2D1::Point2F(1.0f, 1.0f), mTextLayout.Get(), bottomBrush.Get());
+        renderTarget->CreateSolidColorBrush(D2D1::ColorF(mDanmaku->mTextShadowColor, 1.0f), &bottomBrush);
+
+        renderTarget->DrawTextLayout(D2D1::Point2F(0.0f, 1.0f), mTextLayout.Get(), bottomBrush.Get());
+        renderTarget->DrawTextLayout(D2D1::Point2F(2.0f, 1.0f), mTextLayout.Get(), bottomBrush.Get());
+        renderTarget->DrawTextLayout(D2D1::Point2F(1.0f, 0.0f), mTextLayout.Get(), bottomBrush.Get());
+        renderTarget->DrawTextLayout(D2D1::Point2F(1.0f, 2.0f), mTextLayout.Get(), bottomBrush.Get());
 
         ComPtr<ID2D1SolidColorBrush> brush;
         renderTarget->CreateSolidColorBrush(D2D1::ColorF(mDanmaku->mTextColor), &brush);
-        renderTarget->DrawTextLayout(D2D1::Point2F(), mTextLayout.Get(), brush.Get());
+        renderTarget->DrawTextLayout(D2D1::Point2F(1.0f, 1.0f), mTextLayout.Get(), brush.Get());
 
         HRESULT hr = renderTarget->EndDraw();
 
